145076653fa885e2fa3352a5d4f389eca489e96a
Paulo Calueto Francisco <paulo.calueto.francisco@gmail.com>
2023-09-20 22:01:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const headers = document.querySelectorAll(".accordion-header")
+const accordionItem = document.querySelectorAll(".accordion-item")
+
+    headers.forEach(function(header){
+        header.addEventListener("click", function(){
+            
+            const item = this.parentNode
+            
+            const isActive = item.classList.contains("accordion-item-active")
+
+            accordionItem.forEach(function(item){
+                item.classList.remove("accordion-item-active")
+                item.classList.add("accordion-item-closed")
+            })
+
+            if(!isActive){
+                item.classList.add("accordion-item-active")
+                item.classList.remove("accordion-item-closed")
+            }
+        })
+    })
